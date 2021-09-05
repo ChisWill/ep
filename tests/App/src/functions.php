3737e4a97fb1ee82cbaa922b1ac00019a8b35462
CHISWILL-HOST\freed <freedomyky@163.com>
2021-09-05 12:28:08 +0800

    v0.20.2

--- tests/App/src/functions.php
+++ tests/App/src/functions.php
@@ -2,16 +2,18 @@
 
 declare(strict_types=1);
 
-function import(string $name, string $ext = '.php'): array
-{
-    $dir = dirname(__DIR__) . '/config';
+if (!function_exists('import')) {
+    function import(string $name, string $ext = '.php'): array
+    {
+        $dir = dirname(__DIR__) . '/config';
 
-    $configPath = $dir . '/' . $name . $ext;
-    $localConfigPath = $dir . '/' . $name . '-local' . $ext;
+        $configPath = $dir . '/' . $name . $ext;
+        $localConfigPath = $dir . '/' . $name . '-local' . $ext;
 
-    if (file_exists($localConfigPath)) {
-        return array_merge(require($configPath), require($localConfigPath));
-    } else {
-        return require($configPath);
+        if (file_exists($localConfigPath)) {
+            return array_merge(require($configPath), require($localConfigPath));
+        } else {
+            return require($configPath);
+        }
     }
 }
